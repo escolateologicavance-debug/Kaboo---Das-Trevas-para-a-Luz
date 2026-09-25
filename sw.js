321efe244b904fd4718d9a077ea73453268cb175
Gilberto Moreira <escolateologicavance@gmail.com>
2026-09-25 11:46:59 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'kaboo-v2';
+const ASSETS = [
+'./index.html',
+'./manifest.json',
+'./capa-kaboo.jpg',
+'./logo-192.png',
+'./logo-512.png'
+];
+
+self.addEventListener('install', (event) => {
+event.waitUntil(
+caches.open(CACHE_NAME).then((cache) => {
+return cache.addAll(ASSETS);
+})
+);
+self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+event.waitUntil(
+caches.keys().then((keys) => {
+return Promise.all(
+keys.map((key) => {
+if (key !== CACHE_NAME) {
+return caches.delete(key);
+}
+})
+);
+})
+);
+self.clientsClaim();
+});
+
+self.addEventListener('fetch', (event) => {
+event.respondWith(
+caches.match(event.request).then((cachedResponse) => {
+return cachedResponse || fetch(event.request);
+})
+);
+});
